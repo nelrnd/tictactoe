36250e52cba58f2d666c0f1e26c8ae35e4725b95
Nel <nel.renaudin@gmail.com>
2024-06-01 20:36:47 +0200

    integrate chakra ui and create home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,51 @@
+import { Box, Button, Center, Container, Heading, Input, Stack } from "@chakra-ui/react"
+import { useState } from "react"
+
 function App() {
+  const [username, setUsername] = useState("")
+  const [stage, setStage] = useState(0)
+
+  function handleContinue(event) {
+    event.preventDefault()
+
+    if (username.trim() === "") {
+      setUsername("")
+      return
+    }
+
+    setStage((stage) => ++stage)
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Center h="100vh">
+      <Box>
+        <Heading as="h1" size="4xl" textAlign="center" paddingBottom="2rem">
+          TicTacToe
+        </Heading>
+
+        <Container>
+          {stage === 0 && (
+            <form onSubmit={handleContinue}>
+              <Stack>
+                <Input
+                  placeholder="Your username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+                <Button>Continue</Button>
+              </Stack>
+            </form>
+          )}
+          {stage === 1 && (
+            <Stack>
+              <Button>Play</Button>
+              <Button>Create private room</Button>
+            </Stack>
+          )}
+        </Container>
+      </Box>
+    </Center>
   )
 }
 
