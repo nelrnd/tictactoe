6bab09ba7f044d7e2a1c49180dbdb465a53fd95d
Nel <nel.renaudin@gmail.com>
2024-06-08 03:04:18 +0200

    update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client"
 
 const SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL
 
-const URL = import.meta.env.NODE_ENV === "production" ? SERVER_BASE_URL : "http://localhost:3000"
+const URL = import.meta.env.MODE === "production" ? SERVER_BASE_URL : "http://localhost:3000"
 
 export const socket = io(URL)
